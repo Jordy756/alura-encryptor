4fcdf59a2b40f84e99211b2409f2c3ca10373fc7
Jordy756 <jordycastro1756@gmail.com>
2025-12-11 02:13:52 -0600

    feat(4G-123): add encryption and clipboard handling functionality

--- src/scripts/handleEncryption.ts
+++ src/scripts/handleEncryption.ts
@@ -3,14 +3,17 @@ import { encrypt, decrypt } from "@utils/encryptor";
 const initEncryption = () => {
     const encryptButton = document.getElementById("btn-encrypt") as HTMLButtonElement;
     const decryptButton = document.getElementById("btn-decrypt") as HTMLButtonElement;
+    const copyButton = document.getElementById("btn-copy-to-clipboard") as HTMLButtonElement;
     const outputField = document.getElementById("output-text") as HTMLSpanElement;
 
     const getText = () => (document.getElementById("input-text") as HTMLInputElement).value;
     const handleEncrypt = () => encrypt(outputField, getText());
     const handleDecrypt = () => decrypt(outputField, getText());
+    const copyToClipboard = () => navigator.clipboard.writeText(outputField.textContent);
 
     encryptButton.addEventListener("click", handleEncrypt);
     decryptButton.addEventListener("click", handleDecrypt);
+    copyButton.addEventListener("click", copyToClipboard);
 };
 
 initEncryption();
