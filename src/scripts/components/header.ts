afcc071322ecf7c5fb1cb10f2ec064ae22afc927
Jordy756 <jordycastro1756@gmail.com>
2025-12-11 16:06:21 -0600

    style(4G-123): adjust transition duration for button color change

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -2,19 +2,6 @@ const initHeader = () => {
     const htmlRootElement = document.documentElement as HTMLHtmlElement;
     const toggleThemeButton = document.getElementById("toggle-theme-btn") as HTMLButtonElement;
 
-    const theme = (() => {
-        const localStorageTheme = localStorage?.getItem("theme") ?? "";
-
-        if (["dark", "light"].includes(localStorageTheme)) return localStorageTheme;
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) return "dark";
-
-        return "light";
-    })();
-
-    if (theme === "light") htmlRootElement.classList.add("light");
-
-    window.localStorage.setItem("theme", theme);
-
     const handleToggle = () => {
         htmlRootElement.classList.toggle("light");
 
