9c5680c02b6207a038c8c44c65d5b7c24205075d
Jordy756 <jordycastro1756@gmail.com>
2025-12-12 03:10:30 -0600

    style(4G-123): update button transition effects for smoother interactions

--- src/scripts/sections/seeText.ts
+++ src/scripts/sections/seeText.ts
@@ -1,8 +1,19 @@
 const initSeeText = (): void => {
     const copyButton = document.getElementById("btn-copy-to-clipboard") as HTMLButtonElement;
+    const copyTextButton = copyButton.querySelector("span") as HTMLSpanElement;
     const outputField = document.getElementById("output-text") as HTMLSpanElement;
 
-    const copyToClipboard = () => navigator.clipboard.writeText(outputField.textContent);
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(outputField.textContent);
+
+        copyTextButton.textContent = "Copiado!";
+        copyButton.style.backgroundColor = "var(--success-500)";
+
+        setTimeout(() => {
+            copyTextButton.textContent = "Copiar";
+            copyButton.style.backgroundColor = "var(--primary-500)";
+        }, 1000);
+    };
 
     copyButton.addEventListener("click", copyToClipboard);
 };
